c49b2abb36bf23505e6b0b4603a143a950398f5c
estagent <estagent@chip.me>
2020-12-28 07:31:42 +0300

    function names updated

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-import {mergeTranslations} from "@revgaming/languages"
-import {getTimeZones, setTimeZoneCode, detectTimeZone} from "./timezone";
-import {getCountries, getCountry, getCountryName} from "./country";
-import translations from "./translations";
-import {config} from "@revgaming/config";
+import {mergeTranslations} from '@revgaming/languages'
+import {getTimeZones, setTimeZoneCode, detectTimeZone} from './timezone'
+import {getCountries, getCountry, getCountryName} from './country'
+import translations from './translations'
+import {config} from '@revgaming/config'
 
-
-const timezone = () => config('app.timezone');
+const timezone = () => config('app.timezone')
 
 export const bootLocation = opts => {
-    if (opts.hasOwnProperty("tz"))
-        setTimeZoneCode(opts.tz, false)
-    else
-        detectTimeZone();
-    mergeTranslations('location', translations)
-
-    return {
-        timezone: timezone,
-    }
+  if (opts.hasOwnProperty('tz')) setTimeZoneCode(opts.tz, false)
+  else detectTimeZone()
+  mergeTranslations('location', translations)
+  return {
+    timezone: timezone,
+  }
 }
 
 export {
-    timezone,
-    setTimeZoneCode as setTimezone,
-    getTimeZones as timezones,
-    getCountry as country,
-    getCountries as countries,
-    getCountryName as country_name
+  timezone,
+  setTimeZoneCode as setTimezone,
+  getTimeZones as timezones,
+  getCountry as country,
+  getCountries as countries,
+  getCountryName as country_name,
 }
